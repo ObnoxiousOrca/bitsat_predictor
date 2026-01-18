72c622b0af0fca5a4557178d94cc986cc071135d
Orca <darkdragonlord30805@gmail.com>
2026-01-19 04:27:12 +0530

    changed from jsx to tsx

--- bitsat_branch_predictor/src/lib/utils.tsx
+++ bitsat_branch_predictor/src/lib/utils.tsx
@@ -1,12 +1,6 @@
 import srihari from '@/assets/team/Golden-purple.png';
 import pranav from '@/assets/team/PranavU-Coder.png';
 
-
-export const TEAM_MEMBERS: TeamMemberProps[] = [
-    { image: pranav, name: "Pranav", role: "Lead", githubLink: "https://github.com/PranavU-Coder", description: 'Progenitor of the idea, created the ML-model for predictions and contributed to the website.' },
-    { image: srihari, name: "Golden Purple", role: "Co-Lead", githubLink: "https://github.com/Golden-purple", description: 'Helped in model-tuning and creator of the website.' },
-] as const;
-
 export type TeamMemberProps = {
     image: string;
     name: string;
@@ -14,3 +8,8 @@ export type TeamMemberProps = {
     githubLink: string;
     description: string;
 };
+
+export const TEAM_MEMBERS: TeamMemberProps[] = [
+    { image: pranav, name: "Pranav", role: "Lead", githubLink: "https://github.com/PranavU-Coder", description: 'Progenitor of the idea, created the ML-model for predictions and contributed to the website.' },
+    { image: srihari, name: "Golden Purple", role: "Co-Lead", githubLink: "https://github.com/Golden-purple", description: 'Helped in model-tuning and creator of the website.' },
+] as const;
